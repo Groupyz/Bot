67de315097a613a716a568bb8cef27511a73d4d8
stavtasa <stavtasa@gmail.com>
2023-07-31 17:48:59 +0300

    return qr ad dataurl

--- whats-app-bot-events/routes.js
+++ whats-app-bot-events/routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth } = require("whatsapp-web.js");
+const QRCode = require("qrcode");
 
 router.use(express.json());
 
 router.get("/qr", (req, res) => {
+
+  //create a new client
   const client = new Client({
     authStrategy: new LocalAuth({
       dataPath: "../",
@@ -17,17 +19,36 @@ router.get("/qr", (req, res) => {
     },
   });
 
+  //create qr code for client
   client.on("qr", (qr) => {
-    qrcode.generate(qr, { small: true });
-  });
 
+  //qr code image parameters
+  var opts = {
+    errorCorrectionLevel: 'H',
+    type: 'image/jpeg',
+    quality: 0.3,
+    margin: 1,
+    color: {
+      dark:"#000000",
+      light:"#FFFFFFFF"
+    }
+  }
+  
+  //convert qr to DataURL
+  QRCode.toDataURL(qr, opts, function (err, url) {
+    if (err) throw err
+  
+    res.send(url);
+  })
+});
+
+  //happens after client has scaned the qr code
   client.on("ready", () => {
     console.log("Client is ready!");
   });
 
   client.initialize().catch(console.error);
 
-  res.send("Hello qr!");
 });
 
 
