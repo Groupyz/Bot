1928f4671a9f35ee974200438c856c1008b583e0
stavtasa <stavtasa@gmail.com>
2023-08-01 14:02:44 +0300

    seperate to functions

--- whats-app-bot-events/routes.js
+++ whats-app-bot-events/routes.js
@@ -5,51 +5,49 @@ const QRCode = require("qrcode");
 
 router.use(express.json());
 
-router.get("/qr", (req, res) => {
-
-  //create a new client
-  const client = new Client({
-    authStrategy: new LocalAuth({
-      dataPath: "../",
-    }),
-    puppeteer: {
-      headless: false,
-      args: ["--no-sandbox"],
-      browserWSEndpoint: process.env.BROWSER_URL,
-    },
-  });
-
-  //create qr code for client
-  client.on("qr", (qr) => {
-
-  //qr code image parameters
-  var opts = {
-    errorCorrectionLevel: 'H',
-    type: 'image/jpeg',
-    quality: 0.3,
-    margin: 1,
-    color: {
-      dark:"#000000",
-      light:"#FFFFFFFF"
-    }
-  }
-  
-  //convert qr to DataURL
-  QRCode.toDataURL(qr, opts, function (err, url) {
-    if (err) throw err
-  
-    res.send(url);
-  })
+var client = new Client({
+  authStrategy: new LocalAuth({
+    dataPath: "../",
+  }),
+  puppeteer: {
+    headless: false,
+    args: ["--no-sandbox"],
+    browserWSEndpoint: process.env.BROWSER_URL,
+  },
 });
 
-  //happens after client has scaned the qr code
+router.get("/qr", (req, res) => {
+  client.on("qr", (qr) => {
+    //qr code image parameters
+    var opts = init_image_attrubtes();
+
+    //convert qr to DataURL
+    QRCode.toDataURL(qr, opts, function (err, url) {
+      if (err) throw err;
+
+      res.send(url);
+    });
+  });
+
+  //happens after client has scanned the qr code
   client.on("ready", () => {
     console.log("Client is ready!");
   });
 
   client.initialize().catch(console.error);
-
 });
 
+init_image_attrubtes = () => {
+  return {
+    errorCorrectionLevel: "H",
+    type: "image/jpeg",
+    quality: 0.3,
+    margin: 1,
+    color: {
+      dark: "#000000",
+      light: "#FFFFFFFF",
+    },
+  };
+};
 
 module.exports = router;
